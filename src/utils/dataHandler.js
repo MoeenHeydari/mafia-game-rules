3e0aa1fe9b0ce6672d3a5fa1329bd387d9f0b7e0
Moeen Heydari <moeentk@gmail.com>
2023-03-04 11:46:15 +0100

    230304: adding some little changes

--- src/utils/dataHandler.js
+++ src/utils/dataHandler.js
@@ -1,27 +1,27 @@
-const data = require('../data/data.json')
+const data = require('../data/data.json');
 
 // The function to create the list of roles of town, mafia or neutral
 function getRoles(type) {
-    const roles = data['roles'][type]
+    const roles = data['roles'][type];
     return roles
 }
 
 // The function to check which card should be shown
 function getCards(name, type) { // e.g. (citizen, town)
-    const card = []
-    const cardName = name
-    const roleId = getRoles(type).find(role => role.name === cardName).id
-    const checkedCard = getRoles(type).filter((role)=> role.name === cardName)[0]
+    const card = [];
+    const cardName = name;
+    const roleId = getRoles(type).find(role => role.name === cardName).id;
+    const checkedCard = getRoles(type).filter((role)=> role.name === cardName)[0];
     card.push(checkedCard)
     return {card, roleId}
 }
 
 // The function to create the list of alignments
 function getAlignments(type) {
-    const alignments = []
-    const newAlignment = []
+    const alignments = [];
+    const newAlignment = [];
     for ( var i = 0 ; i < getRoles(type).length ; i++ ) {
-        const exists = newAlignment.includes(getRoles(type)[i].alignment)
+        const exists = newAlignment.includes(getRoles(type)[i].alignment);
         // To remove repeated alignments
         if (!exists) {
             newAlignment.push(getRoles(type)[i].alignment)
@@ -33,7 +33,7 @@ function getAlignments(type) {
 
 // The function to create the list of classifications
 function getClassifications() {
-    const classifications = data['classifications']
+    const classifications = data['classifications'];
     return classifications
 }
 
@@ -45,12 +45,16 @@ function goUpperPage(classification) {
 // The function for navigation through roles pages
 function shiftRolesPage(index, x){
     
-    const newIndex = index + x
+    const newIndex = index + x;
     if (newIndex === 0 || newIndex === 3){
         window.location = 'townRoles'
-    } if (newIndex === 1) {
+    }
+    
+    if (newIndex === 1) {
         window.location = 'mafiaRoles'
-    } if (newIndex === 2 || newIndex === -1) {
+    }
+    
+    if (newIndex === 2 || newIndex === -1) {
         window.location = 'neutralRoles'
     }
 } 
@@ -58,7 +62,7 @@ function shiftRolesPage(index, x){
 // The function for navigation through card pages
 function changeCardPage(id, x, type) {
     const newId = id + x;
-    const newRole = getRoles(type).find(role => role.id === newId).name
+    const newRole = getRoles(type).find(role => role.id === newId).name;
     window.location = newRole
 }
 
